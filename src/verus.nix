cd2430c8a9826e12ff165cf8634614299d7581e4
George Miao <gm@miao.dev>
2026-03-04 12:13:58 -0500

    feat(verus): overridable toolchain

--- src/verus.nix
+++ src/verus.nix
@@ -1,4 +1,5 @@
 {
+  toolchain ? "nightly-x86_64-nixos-nixos",
   stdenvNoCC,
   writeTextFile,
   generated,
@@ -11,35 +12,33 @@ let
     """
     Spoof rustup.
     """
+
     import argparse
     import subprocess
 
-    triple = "nightly-x86_64-nixos-nixos"
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+    parser_show = subparsers.add_parser("show")
+    parser_show.add_argument("show")
+    parser_toolchain = subparsers.add_parser("toolchain")
+    parser_toolchain.add_argument("toolchain")
+    parser_run = subparsers.add_parser("run")
+    parser_run.add_argument("run", nargs="*")
+    args = parser.parse_args()
 
-    if __name__ == "__main__":
-        parser = argparse.ArgumentParser()
-        subparsers = parser.add_subparsers()
-        parser_show = subparsers.add_parser("show")
-        parser_show.add_argument("show")
-        parser_toolchain = subparsers.add_parser("toolchain")
-        parser_toolchain.add_argument("toolchain")
-        parser_run = subparsers.add_parser("run")
-        parser_run.add_argument("run", nargs="*")
-        args = parser.parse_args()
-
-        if "show" in args and args.show == "active-toolchain":
-            print(triple)
-        elif "toolchain" in args and args.toolchain == "list":
-            print(triple)
-        elif "run" in args:
-            subprocess.run(args.run[1:])
+    if "show" in args and args.show == "active-toolchain":
+        print("${toolchain}")
+    elif "toolchain" in args and args.toolchain == "list":
+        print("${toolchain}")
+    elif "run" in args:
+        subprocess.run(args.run[1:])
   '';
   rustup = writeTextFile {
     name = "rustup";
     text = rustupSrc;
     executable = true;
   };
-  src =
+  generatedSrc =
     if system == "x86_64-linux" then
       generated.verus-linux-x64
     else if system == "aarch64-darwin" then
@@ -51,7 +50,7 @@ let
 in
 stdenvNoCC.mkDerivation {
   pname = "verus";
-  inherit (src) version src;
+  inherit (generatedSrc) version src;
 
   installPhase = ''
     mkdir -p $out
